4fc520899bbd85bb8386742b96830ff3a5226c6e
bhclira <brenohencastro@gmail.com>
2025-02-07 15:06:40 -0300

    Atualiza informações de perfil e ajusta a estrutura do HTML para exibição dinâmica

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,29 @@
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo');
+    photo.src= profileData.photo;
+    photo.alt= profileData.name;
+
+    const profileName = document.getElementById('profile.name');
+    profileName.textContent = profileData.name;
+
+    const profileJob = document.getElementById('profile.job');
+    profileJob.textContent = profileData.job;
+
+    const profileLocation = document.getElementById('profile.location');
+    profileLocation.textContent = profileData.location;
+
+    const profilePhone = document.getElementById('profile.phone');
+    profilePhone.textContent = profileData.phone;
+
+    const profileEmail = document.getElementById('profile.email');
+    profileEmail.textContent = profileData.email;
+}
 
 
 (async () => {
 
     const profileData = await fetchProfileData()
-    console.log(profileData)
+    updateProfileInfo(profileData)
 
 })();
 

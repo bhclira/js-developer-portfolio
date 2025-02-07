1b2094ad4cd8745279fce0f19c59a89f3728db8d
bhclira <brenohencastro@gmail.com>
2025-02-07 18:10:52 -0300

    Adiciona função para atualizar portfólio e corrige chamada da função no carregamento de dados do perfil.

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,6 +45,18 @@ function updatePortifolio(profileData) {
     portifolio.innerHTML = profileData.portifolio.map(portifolio => `<li>${portifolio}</li>`).join('')
 }
 
+function updatePortfolio (profileData) {
+    const portfolio = document.getElementById('profile.portfolio')
+    portfolio.innerHTML = profileData.portfolio.map(project => {
+        return `
+            <li>
+                <h3>${project.github ? 'class="github"' : ''}>${project.name}</h3>
+                <a href="${project.url}" target="_blank">${project.url}</a>
+            </li>
+        `
+    }).join('')
+}
+
 (async () => {
 
     const profileData = await fetchProfileData()
@@ -52,6 +64,7 @@ function updatePortifolio(profileData) {
     updateSoftSkills(profileData)
     updateHardSkills(profileData)
     updateLanguages(profileData)
+    updatePortifolio(profileData)
 
 })();
 
